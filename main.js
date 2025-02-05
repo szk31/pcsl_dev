1fcddae4a7484f58fc82d552b035b6cadea5736a
Anson <84370085+szk31@users.noreply.github.com>
2025-02-06 01:44:07 +0900

    1.8.4 test4
    PWA test

--- main.js
+++ main.js
@@ -84,7 +84,7 @@ const entry_idx = {
 
 let video, entry;
 
-const version = "1.8.4 test3";
+const version = "1.8.4 test4";
 const key_hash = [
 	"473c05c1ae8349a187d233a02c514ac73fe08ff4418429806a49f7b2fe4ba0b7a36ba95df1d58b8e84a602258af69194", //thereIsNoPassword
 	"3f01e53f1bcee58f6fb472b5d2cf8e00ce673b13599791d8d2d4ddcde3defbbb4e0ab7bc704538080d704d87d79d0410"
@@ -692,7 +692,7 @@ $(function() {
 		
 		// menu - install
 		$(document).on("click", "#menu_install:not(.disabled)", async function() {
-			$("#__log__").html(typeof deferred_prompt, deferred_prompt?.toString());
+			$("#__log__").text(JSON.stringify(Object.keys(deferred_prompt)));
 			if (!deferred_prompt) {
 				return;
 			}
