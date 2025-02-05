b3d443c2a2d6b9a96325f063e225dd09feb7d2ab
Anson <84370085+szk31@users.noreply.github.com>
2025-02-06 02:29:54 +0900

    1.8.4 test8
    PWA test

--- main.js
+++ main.js
@@ -84,7 +84,7 @@ const entry_idx = {
 
 let video, entry;
 
-const version = "1.8.4 test7";
+const version = "1.8.4 test8";
 const key_hash = [
 	"473c05c1ae8349a187d233a02c514ac73fe08ff4418429806a49f7b2fe4ba0b7a36ba95df1d58b8e84a602258af69194", //thereIsNoPassword
 	"3f01e53f1bcee58f6fb472b5d2cf8e00ce673b13599791d8d2d4ddcde3defbbb4e0ab7bc704538080d704d87d79d0410"
@@ -698,7 +698,7 @@ $(function() {
 			}
 
 			deferred_prompt.prompt();
-			const result = deferred_prompt.userChoice;
+			const result = await deferred_prompt.userChoice;
 			$("#menu_install").toggleClass("disabled", result.outcome === "accepted");
 			deferred_prompt = null;
 		});
